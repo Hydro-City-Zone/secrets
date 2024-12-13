ad10294d9fb8c3470ab8c4d2984fa2ea67388d4b
FlaringK <caelans@iinet.net.au>
2024-12-13 14:14:48 +0800

    LocalHost

--- str8forward.js
+++ str8forward.js
@@ -1,14 +1,18 @@
+let prefix = "/secrets/"
+if (window.location.href.includes("127.0.0.1")) {
+  prefix = "./"
+}
 
 let enterPassword = async () => {
   const password = document.getElementById("password").value
   console.log(password)
 
-  let page = await fetch(`./${password}/index.html`).then((res) => res.text())
+  let page = await fetch(`${prefix}${password}/index.html`).then((res) => res.text())
 
   if (page.includes("Error")) {
     document.getElementById("Error").innerText = "Try again"
   } else {
     document.getElementById("Error").innerText = ""
-    window.location.href = `./${password}/index.html`;
+    window.location.href = `${prefix}${password}/index.html`;
   }
 }
